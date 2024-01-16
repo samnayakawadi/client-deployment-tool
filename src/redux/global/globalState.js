d415c052ada00f4cb890449a8179b8044bb5053e
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-01-16 18:22:48 +0530

    Added --host to npm run dev command + PRO Server URL Added

--- src/redux/global/globalState.js
+++ src/redux/global/globalState.js
@@ -1,6 +1,6 @@
 export const globalState = {
     servers: {
-        main: "http://10.244.2.206:5000",
+        main: "https://client-deployment-tool.onrender.com",
     },
     time: {
         autoClose: 2000
