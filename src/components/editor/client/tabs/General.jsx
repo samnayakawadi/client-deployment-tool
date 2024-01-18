3dfa836db3b453016ca542d172fcdd85ddd35aac
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-01-18 16:05:47 +0530

    Added Text Field in General Tab

--- src/components/editor/client/tabs/General.jsx
+++ src/components/editor/client/tabs/General.jsx
@@ -1,6 +1,13 @@
 const General = () => {
     return (
-        <div>General</div>
+        <div className="flex flex-row gap-2 items-center">
+            <div className="basis-3/12 text-center">
+                General
+            </div>
+            <div className="basis-9/12">
+                <input type="text" placeholder="Type here" className="input input-bordered input-secondary w-full" />
+            </div>
+        </div>
     )
 }
 
