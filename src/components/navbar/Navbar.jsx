8d37b248b8ea05f4cbf9ac4f2d1f9b973ac91d6a
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-01-19 14:17:15 +0530

    Updated the App Name to DeployMaster

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = ({ currentLocation }) => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 hover:text-redColor">
-                            <li><a href="#home" className={`hover:text-redColor ${currentLocation === "home" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Home</a></li>
+                            {/* <li><a href="#home" className={`hover:text-redColor ${currentLocation === "home" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Home</a></li>
                             <li><a href="#about" className={`hover:text-redColor ${currentLocation === "about" ? "text-redColor" : "text-white"} hover:bg-navbg`}>About</a></li>
                             <li><a href="#service" className={`hover:text-redColor ${currentLocation === "service" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Service</a></li>
                             <li><a href="#project" className={`hover:text-redColor ${currentLocation === "project" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Projects</a></li>
-                            <li><a href="#contact" className={`hover:text-redColor ${currentLocation === "contact" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Contact</a></li>
+                            <li><a href="#contact" className={`hover:text-redColor ${currentLocation === "contact" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Contact</a></li> */}
                             {/* <li>
                                 <a>Parent</a>
                                 <ul className="p-2">
@@ -23,13 +23,13 @@ const Navbar = ({ currentLocation }) => {
                             </li> */}
                         </ul>
                     </div>
-                    <a href="#" className="btn btn-ghost normal-case text-xl hover:bg-navbg max-lg:hidden">Client Deployment Tool</a>
-                    <a href="#" className="btn btn-ghost normal-case text-xl hover:bg-navbg lg:hidden">CDT</a>
+                    <a href="#" className="btn btn-ghost normal-case text-xl hover:bg-navbg max-lg:hidden">Deploy Master</a>
+                    <a href="#" className="btn btn-ghost normal-case text-xl hover:bg-navbg lg:hidden">Deploy Master</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1 font-semibold">
                         <li><a href="#home" className={`hover:text-redColor ${currentLocation === "home" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Documentation</a></li>
-                        <li><a href="#home" className={`hover:text-redColor ${currentLocation === "home" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Support</a></li>
+                        <li><a href="https://meghsikshak.in/" className={`hover:text-redColor ${currentLocation === "home" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Support</a></li>
                         {/* <li><a href="#about" className={`hover:text-redColor ${currentLocation === "about" ? "text-redColor" : "text-white"} hover:bg-navbg`}>About</a></li>
                         <li><a href="#service" className={`hover:text-redColor ${currentLocation === "service" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Service</a></li>
                         <li><a href="#project" className={`hover:text-redColor ${currentLocation === "project" ? "text-redColor" : "text-white"} hover:bg-navbg`}>Projects</a></li>
@@ -46,7 +46,7 @@ const Navbar = ({ currentLocation }) => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a href="http://gg.gg/samnayakawadi-resume" target="_blank" rel="noreferrer" className="btn btn-sm bg-redColor border-redColor text-white hover:bg-redColor hover:border-redColor hover:text-white tracking-widest"><span className="block max-lg:hidden">Download Builds</span><span className="hidden max-lg:block">e-Resume</span></a>
+                    <a href="https://drive.google.com/drive/folders/1NZGwJ0QeR80B7E6hw9VoKlafLGoGk9XX?usp=sharing" target="_blank" rel="noreferrer" className="btn btn-sm bg-redColor border-redColor text-white hover:bg-redColor hover:border-redColor hover:text-white tracking-widest"><span className="block max-lg:hidden">Download Builds</span><span className="hidden max-lg:block">Builds</span></a>
                 </div>
                 {/* <div className="navbar-end max-lg:block">
                     <a href="http://gg.gg/samnayakawadi-resume" target="_blank" rel="noreferrer" className="btn btn-sm bg-redColor border-redColor text-white hover:bg-redColor hover:border-redColor hover:text-white tracking-widest">J</a>
