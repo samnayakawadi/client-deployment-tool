b19760c8130b67bf8fdf53a90d83e509ecc5c62a
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-06-26 15:39:45 +0530

    Added the with/without security mongodbURI

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose"
 
 export const connectDatabase = () => {
 
+    // With Security
     const DB_USERNAME = process.env.mongodb_username
     const DB_PASSWORD = encodeURIComponent(process.env.mongodb_password)
     const DB_DATABASENAME = process.env.mongodb_database
     const DB_CLUSTERNAME = process.env.mongodb_cluster
 
     const mongodbURI = process.env.server_mode === "DEV" ? `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTERNAME}?authSource=${DB_DATABASENAME}` : `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTERNAME}/${DB_DATABASENAME}?retryWrites=true&w=majority`
+
+    // Without Security
+    // const mongodbURI = "mongodb://127.0.0.1:27017/test"
 
     mongoose.connect(mongodbURI).then(res => {
         console.log("MongoDB Connection Successful")
