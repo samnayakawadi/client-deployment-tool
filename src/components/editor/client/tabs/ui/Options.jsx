eac7cc8c0c4cbe9a6c47a0f1bbc18880af54de4c
Samir Nayakawadi <samnayakawadi@gmail.com>
2024-07-01 19:11:42 +0530

    Server Mode Added to UI.Options

--- src/components/editor/client/tabs/ui/Options.jsx
+++ src/components/editor/client/tabs/ui/Options.jsx
@@ -17,7 +17,7 @@ const Options = () => {
     }, [])
 
     return (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-5">
             <div className="flex flex-row gap-2 items-center">
                 <div className="basis-3/12 text-center">
                     Waiting Time (ms)
@@ -53,6 +53,34 @@ const Options = () => {
                 <div className="basis-9/12">
                     {/* <input type="text" name="autoClose" value={options.autoClose} onChange={uiHandlers.updateOptionsTab} placeholder="E.g. 2000" className="input input-bordered input-secondary w-full focus:outline-none" /> */}
                     <input type="checkbox" name="isNewContentDelivery" checked={options.isNewContentDelivery} onClick={uiHandlers.updateOptionsTab} className="toggle toggle-success" />
+                </div>
+            </div>
+
+            <hr className="border-1 border-gray-700" />
+
+            <div className="flex flex-row gap-2 items-center">
+                <div className="basis-3/12 text-center">
+                    Server Mode
+                </div>
+                <div className="basis-9/12">
+                    <div className="flex flex-col gap-3">
+
+                        {/* <input type="radio" name="serverMode" className="radio" onClick={uiHandlers.updateOptionsTab} />
+                        <input type="radio" name="serverMode" className="radio" /> */}
+
+                        <div className="form-control flex flex-row">
+                            <label className="label cursor-pointer gap-3 p-0">
+                                <input type="radio" name="serverMode" value="DEV" onClick={uiHandlers.updateOptionsTab} checked={options.serverMode === "DEV"} className="radio checked:bg-blue-500" />
+                                <span className="label-text">Development</span>
+                            </label>
+                        </div>
+                        <div className="form-control flex flex-row">
+                            <label className="label cursor-pointer gap-3 p-0">
+                                <input type="radio" name="serverMode" value="PRO" onClick={uiHandlers.updateOptionsTab} checked={options.serverMode === "PRO"} className="radio checked:bg-green-500" />
+                                <span className="label-text">Production</span>
+                            </label>
+                        </div>
+                    </div>
                 </div>
             </div>
 
